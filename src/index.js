fe8735b41b0e70a443d8ab77836d6e3f4ab9c6d4
jennyrhee <jennyirhee@gmail.com>
2022-02-25 13:34:39 -0600

    Create task on form submit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,51 @@ import './css/style.css';
 import {Task, Project} from './task.js';
 
 ((doc) => {
-  let _projects = [Project('My Tasks')];
+  const _projects = [Project('My Tasks')];
 
   const _toggleForm = () => {
     doc.getElementById('task-form').classList.toggle('show');
   }
+  const _createTask = (form) => {
+    const newTask = Task(
+      form.elements['task'].value,
+      form.elements['description'].value
+    );
+    _projects[0].addTask(newTask);
+
+    return newTask;
+  }
+  const _addTask = (task, taskContainer) => {
+    const container = doc.createElement('div');
+    container.classList.add('container');
+
+    const check = doc.createElement('input');
+    check.type = 'checkbox';
+    check.id = task.getTitle();
+
+    const label = doc.createElement('label');
+    label.htmlFor = task.getTitle();
+    label.classList.add('task');
+    label.textContent = task.getTitle();
+
+    container.appendChild(check);
+    container.appendChild(label);
+    taskContainer.appendChild(container);
+  }
   const _initForm = () => {
+    const form = doc.getElementById('task-form');
     doc.getElementById('add-task-btn').onclick = _toggleForm;
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const newTask = _createTask(form);
+      _addTask(newTask, doc.querySelector('.task-container'));
+      form.reset();
+    });
+    doc.getElementById('cancel-task-btn').onclick = () => {
+      _toggleForm();
+      form.reset();
+    };
   }
   const _addProjects = (projects) => {
     _projects.forEach((project) => {
--- src/task.js
+++ src/task.js
@@ -1,14 +1,23 @@
 const Task = (title, description, dueDate, project, priority) => {
   let _isCompleted = false;
+
+  const getTitle = () => title;
+  return {
+    getTitle
+  };
 }
 
 const Project = (name) => {
   let _tasks = [];
 
   const getName = () => name;
+  const addTask = (task) => {
+    _tasks.push(task);
+  }
 
   return {
-    getName
+    getName,
+    addTask
   };
 }
 
